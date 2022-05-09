7c0f31c8bb70cbf35bd388ca52ebe3a3e83a62ae
shanto <santo15-10959@diu.edu.bd>
2022-05-09 21:00:23 +0600

    add service section

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -4,6 +4,7 @@ import './Home.css';
 import banner from '../../../Images/banner/banner1.jpg';
 import loom1 from '../../../Images/banner/loom1.jpg';
 import loom2 from '../../../Images/banner/loom2.jpg';
+import service from '../../../Images/banner/service.jpg';
 
 const Home = () => {
     
@@ -17,11 +18,16 @@ const Home = () => {
             <Products></Products>
             </div>
             <div>
-                <h3 className='loomsection'>Our Loom section</h3>
+                <h3 className='loomsection'>Manufacture section </h3>
                 <div className='mt-4'>
                     <img className='w-50' src={loom1} alt="" />
                     <img className='w-50' src={loom2} alt="" />
                 </div>
+            </div>
+            <div className='serviceSection'>
+                <h3 className='service'>Our service</h3>
+                <img style={{height: '350px'}} className='w-100' src={service} alt="" />
+                <p>We have our own service system. We can supply our product to your shop. So you don't neet to warry about it. Just visite our warehouse and make your shop uniqe collection. </p>
             </div>
         </div>
     );
